6db86fd49d5461f27f905907801dad118996ae0a
trunggdungg <le25011985@gmail.com>
2025-02-27 15:10:48 +0700

    create file main.js

---
+++ src/main/resources/static/assets/js/main.js
@@ -0,0 +1,24 @@
+console.log("Hello World!");
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:false,
+    dots:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    mouseDrag:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+
