96e5a2784dfbebbf52dbeff8ec1508d1524e5fea
trunggdungg <le25011985@gmail.com>
2025-02-27 15:08:43 +0700

    create project

---
+++ src/main/java/com/example/web_organic/WebOrganicApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_organic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebOrganicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebOrganicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/web_organic/WebOrganicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_organic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebOrganicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
