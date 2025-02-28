0010ce87c55c64be30c440980a632109e04344f0
trunggdungg <le25011985@gmail.com>
2025-02-28 23:19:22 +0700

    create UserRepository

---
+++ src/main/java/com/example/web_organic/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.web_organic.repository;
+
+import com.example.web_organic.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
