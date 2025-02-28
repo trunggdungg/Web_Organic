6ce4498ee82914c33e7a7fbbc073812a3b196e5c
trunggdungg <le25011985@gmail.com>
2025-02-28 23:17:25 +0700

    create ProductRepository

---
+++ src/main/java/com/example/web_organic/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.example.web_organic.repository;
+
+import com.example.web_organic.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+}
