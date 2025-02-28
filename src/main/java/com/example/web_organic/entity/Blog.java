687be94996e0a86172040f64c9203b2d69dc5506
trunggdungg <le25011985@gmail.com>
2025-02-28 23:07:21 +0700

    create entity Blog

---
+++ src/main/java/com/example/web_organic/entity/Blog.java
@@ -0,0 +1,37 @@
+package com.example.web_organic.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE)
+
+@Entity
+@Table(name = "blogs")
+public class Blog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+    String title;
+    String slug;
+    String content;
+    String description;
+    String thumbnail;
+    Boolean status;
+    LocalDateTime createdAt;
+    LocalDateTime updatedAt;
+    LocalDateTime publishedAt;
+    String category;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    User user;
+}
