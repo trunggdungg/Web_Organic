ea5f3e1e66bffaa163c687d65e93c003ca725f12
trunggdungg <le25011985@gmail.com>
2025-02-28 22:58:14 +0700

    create WebController

---
+++ src/main/java/com/example/web_organic/controller/WebController.java
@@ -0,0 +1,54 @@
+package com.example.web_organic.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class WebController {
+    @GetMapping("/")
+    public String getHomePage() {
+        return "/web/index";
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "/web/login";
+    }
+
+    @GetMapping("/register")
+    public String getRegisterPage() {
+        return "/web/signup";
+    }
+
+    @GetMapping("/product")
+    public String getProductPage() {
+        return "/web/product";
+    }
+
+    @GetMapping("/cart")
+    public String getCartPage() {
+        return "/web/cart";
+    }
+
+    @GetMapping("/blog")
+    public String getBlogPage() {
+        return "/web/blog";
+    }
+
+    @GetMapping("/contact")
+    public String getContactPage() {
+        return "/web/contact";
+    }
+
+    @GetMapping("/profile")
+    public String getProfilePage() {
+        return "/web/info-user";
+    }
+
+    @GetMapping("/promotion")
+    public String getPromotionPage() {
+        return "/web/promotion";
+    }
+}
