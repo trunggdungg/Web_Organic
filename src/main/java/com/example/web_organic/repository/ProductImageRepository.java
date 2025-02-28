ab0796ccac33ea5b155b7eef1011145ac60e54f4
trunggdungg <le25011985@gmail.com>
2025-02-28 23:17:08 +0700

    create ProductImageRepository

---
+++ src/main/java/com/example/web_organic/repository/ProductImageRepository.java
@@ -0,0 +1,9 @@
+package com.example.web_organic.repository;
+
+import com.example.web_organic.entity.ProductImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductImageRepository extends JpaRepository<ProductImage, Integer> {
+}
