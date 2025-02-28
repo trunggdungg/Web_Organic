cce3b0c089ebb25b1377576f05df87f94b3d9b3f
trunggdungg <le25011985@gmail.com>
2025-02-28 23:08:29 +0700

    create entity OrderDetail

---
+++ src/main/java/com/example/web_organic/entity/OrderDetail.java
@@ -0,0 +1,34 @@
+package com.example.web_organic.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.text.DecimalFormat;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE)
+
+@Entity
+@Table(name = "order_details")
+public class OrderDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+    DecimalFormat unitPrice;
+    DecimalFormat subTotal;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_variant_id")
+    ProductVariants productVariant;
+}
