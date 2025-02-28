72aa5f990e374e73113eaa2544d9ac5162a44e8f
trunggdungg <le25011985@gmail.com>
2025-02-28 23:11:32 +0700

    create BrandRepository

---
+++ src/main/java/com/example/web_organic/repository/BrandRepository.java
@@ -0,0 +1,9 @@
+package com.example.web_organic.repository;
+
+import com.example.web_organic.entity.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BrandRepository extends JpaRepository<Brand, Integer> {
+}
