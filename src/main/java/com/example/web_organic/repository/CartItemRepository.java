e3a7dd10f8115320341ab2385d113a6d43372cdb
trunggdungg <le25011985@gmail.com>
2025-02-28 23:11:56 +0700

    create CartItemRepository

---
+++ src/main/java/com/example/web_organic/repository/CartItemRepository.java
@@ -0,0 +1,9 @@
+package com.example.web_organic.repository;
+
+import com.example.web_organic.entity.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Integer> {
+}
