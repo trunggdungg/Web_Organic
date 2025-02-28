e752f8c42167ea57abf0f4fa9186de9e8c79ddef
trunggdungg <le25011985@gmail.com>
2025-02-28 23:12:10 +0700

    create CategoryRepository

---
+++ src/main/java/com/example/web_organic/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.example.web_organic.repository;
+
+import com.example.web_organic.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+}
