d5ca39839c079828c26ccab500d8d6b0eb263eea
trunggdungg <le25011985@gmail.com>
2025-02-28 23:11:15 +0700

    create BlogRepository

---
+++ src/main/java/com/example/web_organic/repository/BlogRepository.java
@@ -0,0 +1,9 @@
+package com.example.web_organic.repository;
+
+import com.example.web_organic.entity.Blog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BlogRepository extends JpaRepository<Blog, Integer> {
+}
