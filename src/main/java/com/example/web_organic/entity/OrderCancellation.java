a758866d48fb1c8e0be61162586feefe6346bc7d
trunggdungg <le25011985@gmail.com>
2025-02-28 23:12:54 +0700

    create entity OrderCancellation

---
+++ src/main/java/com/example/web_organic/entity/OrderCancellation.java
@@ -0,0 +1,36 @@
+package com.example.web_organic.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE)
+
+@Entity
+@Table(name = "order_cancellations")
+public class OrderCancellation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    User user;
+
+    String reason;
+    LocalDateTime cancelledAt;
+}
