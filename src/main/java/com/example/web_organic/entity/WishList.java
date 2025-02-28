8de845c4a419e7355d2d4705897d00f2e44d1f74
trunggdungg <le25011985@gmail.com>
2025-02-28 23:09:29 +0700

    create entity WishList

---
+++ src/main/java/com/example/web_organic/entity/WishList.java
@@ -0,0 +1,33 @@
+package com.example.web_organic.entity;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE)
+
+@Entity
+@Table(name = "wish_lists")
+public class WishList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    User user;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    Product product;
+
+    LocalDateTime createdAt;
+    LocalDateTime updatedAt;
+}
