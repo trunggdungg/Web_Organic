0343620b7333016a769cce67c462dbd478ddad04
trunggdungg <le25011985@gmail.com>
2025-02-28 23:18:15 +0700

    create ReviewRepository

---
+++ src/main/java/com/example/web_organic/repository/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.example.web_organic.repository;
+
+import com.example.web_organic.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
+}
