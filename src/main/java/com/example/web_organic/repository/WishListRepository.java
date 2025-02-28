99a5146192cc2d731f43ea46b8fe45117bce2542
trunggdungg <le25011985@gmail.com>
2025-02-28 23:20:18 +0700

    create WishListRepository

---
+++ src/main/java/com/example/web_organic/repository/WishListRepository.java
@@ -0,0 +1,9 @@
+package com.example.web_organic.repository;
+
+import com.example.web_organic.entity.WishList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WishListRepository extends JpaRepository<WishList, Integer> {
+}
