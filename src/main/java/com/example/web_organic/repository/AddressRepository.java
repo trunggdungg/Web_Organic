10de2cb9865ed7697600882e5eb3820a8179c19b
trunggdungg <le25011985@gmail.com>
2025-03-01 18:47:17 +0700

    add methods to AddressRepository for default address retrieval

--- src/main/java/com/example/web_organic/repository/AddressRepository.java
+++ src/main/java/com/example/web_organic/repository/AddressRepository.java
@@ -2,9 +2,21 @@ package com.example.web_organic.repository;
 
 import com.example.web_organic.entity.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Integer> {
 
+    Optional<Object> findByUserIdAndIsDefaultTrue(Integer id);
+
+    @Query("SELECT a.addressSelected FROM Address a WHERE a.user.id = :userId AND a.isDefault = true")
+    String findAddressSelectedByUserId(@Param("userId") Integer userId);
+
+
+    List<Address> findByIsDefaultTrue();
 }
