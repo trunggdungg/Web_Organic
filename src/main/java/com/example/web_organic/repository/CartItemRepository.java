5d79159dadadb57272fa313d2a53ec3ac3d00080
trunggdungg <le25011985@gmail.com>
2025-03-01 18:48:36 +0700

    add method to CartItemRepository for retrieving cart items by user

--- src/main/java/com/example/web_organic/repository/CartItemRepository.java
+++ src/main/java/com/example/web_organic/repository/CartItemRepository.java
@@ -1,9 +1,13 @@
 package com.example.web_organic.repository;
 
 import com.example.web_organic.entity.CartItem;
+import com.example.web_organic.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Integer> {
+    List<CartItem> findByUser(User user);
 }
