8958e6b8784f1f88e27abffddfa18e32d6e15662
trunggdungg <le25011985@gmail.com>
2025-03-02 23:04:01 +0700

    add: create TokenRepository interface for managing token confirmations

---
+++ src/main/java/com/example/web_organic/repository/TokenRepository.java
@@ -0,0 +1,16 @@
+package com.example.web_organic.repository;
+
+import com.example.web_organic.entity.TokenConfirm;
+import com.example.web_organic.entity.User;
+import com.example.web_organic.modal.Enum.Token_Type;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TokenRepository extends JpaRepository<TokenConfirm, Integer> {
+    Optional<TokenConfirm> findByToken(String token);
+    Optional<TokenConfirm> findByUser(User user);
+    void deleteByToken(String token);
+
+    Optional<TokenConfirm> findByTokenAndTokenType(String token, Token_Type tokenType);
+}
