e572f252fb24bbf29937f43c76a8c34d9e530204
trunggdungg <le25011985@gmail.com>
2025-03-02 23:04:08 +0700

    add: enhance UserRepository with methods to find activated users by email

--- src/main/java/com/example/web_organic/repository/UserRepository.java
+++ src/main/java/com/example/web_organic/repository/UserRepository.java
@@ -4,6 +4,12 @@ import com.example.web_organic.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+//    Optional<Object> findByEmail(String email);
+    Optional<User> findByEmailAndIsActivatedTrue(String email);
+
+    Optional<User> findByEmail(String email);
 }
