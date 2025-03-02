b12ffeb0efa412abc935daba81bc0ffa9168c369
trunggdungg <le25011985@gmail.com>
2025-03-02 23:02:22 +0700

    add: implement custom Toast notifications with SweetAlert2 and Toastr

---
+++ src/main/resources/static/assets/js/sweet.js
@@ -0,0 +1,40 @@
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-right', // Vị trí hiển thị
+    showConfirmButton: false,
+    timer: 3000, // Thời gian hiển thị
+    timerProgressBar: true,
+    showCloseButton: true
+});
+
+// Hàm hiển thị thông báo thành công
+function alertSuccess(message) {
+    Toast.fire({
+        icon: 'success',
+        title: message
+    });
+}
+
+// Hàm hiển thị thông báo lỗi
+function alertError(message) {
+    Toast.fire({
+        icon: 'error',
+        title: message
+    });
+}
+
+// Hàm hiển thị thông báo cảnh báo
+function alertWarning(message) {
+    Toast.fire({
+        icon: 'warning',
+        title: message
+    });
+}
+
+// Hàm hiển thị thông báo thông tin
+function alertInfo(message) {
+    Toast.fire({
+        icon: 'info',
+        title: message
+    });
+}
