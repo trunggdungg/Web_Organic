b79e880c1f11420fc873213ec3e04cee5d07ca1d
trunggdungg <le25011985@gmail.com>
2025-03-02 23:03:22 +0700

    add: implement method to retrieve featured products with ratings above three in ProductService

--- src/main/java/com/example/web_organic/repository/ProductRepository.java
+++ src/main/java/com/example/web_organic/repository/ProductRepository.java
@@ -1,9 +1,21 @@
 package com.example.web_organic.repository;
 
 import com.example.web_organic.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+    @Query("""
+    SELECT p FROM Product p 
+    WHERE (p.isFeatured = true 
+    OR (SELECT AVG(r.rating) FROM Review r WHERE r.product.id = p.id) >= 3)
+      AND p.status = true
+""")
+    //lấy ra các sản phẩm có isfeatures = true hoặc có rating trung bình từ các review >= 3
+    Page<Product> findFeaturedProductsWithRatingAboveThree(Pageable pageable);
+
 }
--- src/main/java/com/example/web_organic/service/ProductService.java
+++ src/main/java/com/example/web_organic/service/ProductService.java
@@ -1,8 +1,28 @@
 package com.example.web_organic.service;
+import com.example.web_organic.entity.Product;
+import com.example.web_organic.repository.ProductRepository;
+import com.example.web_organic.repository.ProductVariantRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private ProductVariantRepository productVariantsRepository;
+    public Page<Product> getProductPopular(int page, int pageSize) {
+        Pageable pageable = PageRequest.of(page - 1, pageSize, Sort.by("createdAt").descending());
+        return productRepository.findFeaturedProductsWithRatingAboveThree(pageable);
+    }
+
+
+
 }
