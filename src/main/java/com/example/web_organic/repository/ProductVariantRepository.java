332546b811793c33aad5d1ef46d1a2488597cd4f
trunggdungg <le25011985@gmail.com>
2025-03-02 23:03:29 +0700

    add: update ProductVariantRepository and ProductVariantService for future implementation

--- src/main/java/com/example/web_organic/repository/ProductVariantRepository.java
+++ src/main/java/com/example/web_organic/repository/ProductVariantRepository.java
@@ -2,8 +2,11 @@ package com.example.web_organic.repository;
 
 import com.example.web_organic.entity.ProductVariants;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductVariantRepository extends JpaRepository<ProductVariants, Integer> {
+
 }
--- src/main/java/com/example/web_organic/service/ProductVariantService.java
+++ src/main/java/com/example/web_organic/service/ProductVariantService.java
@@ -5,4 +5,5 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class ProductVariantService {
+
 }
