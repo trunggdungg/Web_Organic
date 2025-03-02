4222fe0f691c1ed1cab8cc792f5eeb89f0f4b2dc
trunggdungg <le25011985@gmail.com>
2025-03-02 23:04:50 +0700

    add: enhance WebController with user session management and account confirmation feature

--- src/main/java/com/example/web_organic/controller/WebController.java
+++ src/main/java/com/example/web_organic/controller/WebController.java
@@ -1,24 +1,47 @@
 package com.example.web_organic.controller;
 
+import com.example.web_organic.entity.Product;
+import com.example.web_organic.entity.User;
+import com.example.web_organic.modal.response.TokenConfirmMessageResponse;
+import com.example.web_organic.service.ProductService;
+import com.example.web_organic.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
 public class WebController {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private ProductService productService;
     @GetMapping("/")
-    public String getHomePage() {
+    public String getHomePage(@RequestParam(required = false,defaultValue = "1") int page,
+        @RequestParam(required = false,defaultValue = "10") int pageSize, Model model) {
+        model.addAttribute("products", productService.getProductPopular(page, pageSize));
         return "/web/index";
     }
 
     @GetMapping("/login")
-    public String getLoginPage() {
+    public String getLoginPage(HttpServletRequest request) {
+        User user = (User) request.getSession().getAttribute("CURRENT_USER");
+        if (user != null) {
+            return "redirect:/";
+        }
         return "/web/login";
     }
 
     @GetMapping("/register")
-    public String getRegisterPage() {
+    public String getRegisterPage(HttpServletRequest request) {
+        User user = (User) request.getSession().getAttribute("CURRENT_USER");
+        if (user != null) {
+            return "redirect:/";
+        }
         return "/web/signup";
     }
 
@@ -60,6 +83,13 @@ public class WebController {
     @GetMapping("/checkout")
     public String getCheckoutPage() {
         return "/web/checkout";
+    }
+
+    @GetMapping("/xac-thuc-tai-khoan")
+    public String confirmAccount(@RequestParam String token, Model model){
+        TokenConfirmMessageResponse response = userService.verifyAccount(token);
+        model.addAttribute("response",response);
+        return "web/xac-thuc-tai-khoan";
     }
 
 }
