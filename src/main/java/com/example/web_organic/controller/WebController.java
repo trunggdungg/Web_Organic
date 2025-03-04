42acb48ea704f4d2e753eb3092ba026e0514e6d8
trunggdungg <le25011985@gmail.com>
2025-03-04 23:49:26 +0700

    add: enhance WebController to include product stock and variant prices in home page model

--- src/main/java/com/example/web_organic/controller/WebController.java
+++ src/main/java/com/example/web_organic/controller/WebController.java
@@ -8,10 +8,16 @@ import com.example.web_organic.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,7 +29,34 @@ public class WebController {
     @GetMapping("/")
     public String getHomePage(@RequestParam(required = false,defaultValue = "1") int page,
         @RequestParam(required = false,defaultValue = "10") int pageSize, Model model) {
-        model.addAttribute("products", productService.getProductPopular(page, pageSize));
+
+        Page<Product> products = productService.getProductPopular(page, pageSize);
+        Page<Product> productCategory = productService.getProductByCategory(1, 10,6);
+
+        // Thêm thông tin stock và giá cho mỗi sản phẩm
+        Map<Integer, Integer> productStocks = new HashMap<>();
+        Map<Integer, BigDecimal> productVariantPrices = new HashMap<>();
+
+        // Hàm dùng chung để thêm dữ liệu vào productStocks & productVariantPrices
+        Consumer<Product> addProductInfo = product -> {
+            int productId = product.getId();
+            productStocks.putIfAbsent(productId, productService.getProductStock(productId)); // Tránh ghi đè nếu đã có
+            productVariantPrices.putIfAbsent(productId, productService.getProductVariantPrice(productId)); // Tránh ghi đè nếu đã có
+        };
+
+        // Lấy stock và giá cho sản phẩm phổ biến
+        products.getContent().forEach(addProductInfo);
+        // Lấy stock và giá cho sản phẩm theo danh mục
+        productCategory.getContent().forEach(addProductInfo);
+
+        // Lấy stock và giá cho sản phẩm ăn ngay
+//      productReadyToEat.getContent().forEach(addProductInfo);
+        model.addAttribute("products", products); //sản phẩm phổ biến
+        model.addAttribute("productStocks", productStocks); //số lượng sản phẩm
+        model.addAttribute("productVariantPrices", productVariantPrices); //giá sản phẩm
+        model.addAttribute("productCategory", productCategory); //sản phẩm theo category
+
+
         return "/web/index";
     }
 
