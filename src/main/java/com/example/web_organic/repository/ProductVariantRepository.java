1edb980ac3b87acc2293fea83b9410d16c084d5a
trunggdungg <le25011985@gmail.com>
2025-03-05 23:41:26 +0700

    add: implement methods to find product variants by product ID in ProductVariantRepository

--- src/main/java/com/example/web_organic/repository/ProductVariantRepository.java
+++ src/main/java/com/example/web_organic/repository/ProductVariantRepository.java
@@ -6,7 +6,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductVariantRepository extends JpaRepository<ProductVariants, Integer> {
 
+    List<ProductVariants> findByProductId(Integer productId);
+
+//    lấy bien tể tru của san pham
+    ProductVariants findByProductIdAndIsDefaultTrue(Integer productId);
 }
