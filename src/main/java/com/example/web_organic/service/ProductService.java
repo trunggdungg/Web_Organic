4553888143eeb72f13f772b08466c5bfe0bee95b
trunggdungg <le25011985@gmail.com>
2025-03-05 23:41:18 +0700

    add: implement methods to retrieve product variant price, stock, and variants in ProductService

--- src/main/java/com/example/web_organic/service/ProductService.java
+++ src/main/java/com/example/web_organic/service/ProductService.java
@@ -1,5 +1,7 @@
 package com.example.web_organic.service;
+
 import com.example.web_organic.entity.Product;
+import com.example.web_organic.entity.ProductVariants;
 import com.example.web_organic.repository.ProductRepository;
 import com.example.web_organic.repository.ProductVariantRepository;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +12,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ProductService {
@@ -17,12 +22,39 @@ public class ProductService {
     private ProductRepository productRepository;
 
     @Autowired
-    private ProductVariantRepository productVariantsRepository;
+    private ProductVariantService productVariantService;
+
+    @Autowired
+    private ProductVariantRepository productVariantRepository;
+
+    //    lấy sản phẩm phổ biến theo rating + số lượng đánh giá>=3
     public Page<Product> getProductPopular(int page, int pageSize) {
         Pageable pageable = PageRequest.of(page - 1, pageSize, Sort.by("createdAt").descending());
         return productRepository.findFeaturedProductsWithRatingAboveThree(pageable);
     }
 
+    //    lấy số lượng sản phẩm theo category ,
+    public Page<Product> getProductByCategory(int page, int pageSize, String slug) {
+        Pageable pageable = PageRequest.of(page - 1, pageSize, Sort.by("createdAt").descending());
+        return productRepository.findByCategorySlug(slug, pageable);
+    }
 
+    // lấy giá sản phẩm
+    public BigDecimal getProductVariantPrice(Integer productId) {
+        ProductVariants variant = productVariantRepository.findByProductIdAndIsDefaultTrue(productId);
+        if (variant == null) {
+            throw new RuntimeException("Không tìm thấy biến thể mặc định cho sản phẩm có ID: " + productId);
+        }
+        return variant.getPrice();
+    }
 
+    // Lấy tổng stock của một sản phẩm
+    public Integer getProductStock(Integer productId) {
+        return productVariantService.getTotalStockByProductId(productId);
+    }
+
+    // Lấy danh sách variants của một sản phẩm
+    public List<ProductVariants> getProductVariants(Integer productId) {
+        return productVariantService.getVariantsByProductId(productId);
+    }
 }
