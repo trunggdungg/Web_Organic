83cf532e2106a31d2baba147d25085c0b302d402
trunggdungg <le25011985@gmail.com>
2025-03-06 23:42:06 +0700

    add: implement method to find blogs by status with pagination in BlogRepository

--- src/main/java/com/example/web_organic/repository/BlogRepository.java
+++ src/main/java/com/example/web_organic/repository/BlogRepository.java
@@ -1,9 +1,12 @@
 package com.example.web_organic.repository;
 
 import com.example.web_organic.entity.Blog;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BlogRepository extends JpaRepository<Blog, Integer> {
+    Page<Blog> findByStatus(boolean b, PageRequest createdAt);
 }
