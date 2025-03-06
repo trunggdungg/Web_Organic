679989b079e44e3a18026c11baff6297696e5525
trunggdungg <le25011985@gmail.com>
2025-03-06 23:42:49 +0700

    add: implement method to retrieve popular blogs with pagination in BlogService

--- src/main/java/com/example/web_organic/service/BlogService.java
+++ src/main/java/com/example/web_organic/service/BlogService.java
@@ -1,9 +1,21 @@
 package com.example.web_organic.service;
 
+import com.example.web_organic.entity.Blog;
+import com.example.web_organic.repository.BlogRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class BlogService {
+    @Autowired
+    private BlogRepository blogRepository;
+
+    public Page<Blog> getBlogPopular(int page, int pageSize) {
+        return blogRepository.findByStatus(true, PageRequest.of(page - 1, pageSize, Sort.by("createdAt").descending()));
+    }
 }
