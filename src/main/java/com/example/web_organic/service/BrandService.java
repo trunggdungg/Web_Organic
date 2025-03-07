e22fdd01109aa631c5177241443b1e17dfcbef1e
trunggdungg <le25011985@gmail.com>
2025-03-07 23:59:34 +0700

    add: implement method to retrieve all brands in BrandService

--- src/main/java/com/example/web_organic/service/BrandService.java
+++ src/main/java/com/example/web_organic/service/BrandService.java
@@ -1,8 +1,19 @@
 package com.example.web_organic.service;
+import com.example.web_organic.entity.Brand;
+import com.example.web_organic.repository.BrandRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class BrandService {
+    @Autowired
+    private BrandRepository brandRepository;
+
+    public List<Brand> getAllBrands() {
+        return brandRepository.findAll();
+    }
 }
