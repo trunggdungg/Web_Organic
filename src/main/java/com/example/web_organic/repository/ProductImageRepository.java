9d7da67f842986bcaaf06a62404c87257c04c5bb
trunggdungg <le25011985@gmail.com>
2025-03-08 23:45:47 +0700

    add: implement method to find product images by product ID in ProductImageRepository

--- src/main/java/com/example/web_organic/repository/ProductImageRepository.java
+++ src/main/java/com/example/web_organic/repository/ProductImageRepository.java
@@ -4,6 +4,9 @@ import com.example.web_organic.entity.ProductImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductImageRepository extends JpaRepository<ProductImage, Integer> {
+    List<ProductImage> findByProductId(Integer id);
 }
