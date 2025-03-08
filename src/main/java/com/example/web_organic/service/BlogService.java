17a8b6cd10ac4a8040dbe0805e3c9ab5b7338a35
trunggdungg <le25011985@gmail.com>
2025-03-08 23:46:44 +0700

    add: implement methods to retrieve blogs by category and to fetch all blogs

--- src/main/java/com/example/web_organic/service/BlogService.java
+++ src/main/java/com/example/web_organic/service/BlogService.java
@@ -1,6 +1,7 @@
 package com.example.web_organic.service;
 
 import com.example.web_organic.entity.Blog;
+import com.example.web_organic.entity.Category;
 import com.example.web_organic.repository.BlogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,5 +20,14 @@ public class BlogService {
 
     public Page<Blog> getBlogPopular(int page, int pageSize) {
         return blogRepository.findByStatus(true, PageRequest.of(page - 1, pageSize, Sort.by("createdAt").descending()));
+    }
+
+
+    public List<Blog> getBlogsByCategory(Category selectedCategory) {
+        return blogRepository.findByCategoryAndStatusTrue(selectedCategory);
+    }
+
+    public List<Blog> getAllBlogs(int page, int pageSize) {
+        return blogRepository.findAll(PageRequest.of(page - 1, pageSize, Sort.by("createdAt").descending())).getContent();
     }
 }
