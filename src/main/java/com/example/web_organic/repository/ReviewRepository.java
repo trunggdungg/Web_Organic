28ee83125a763ebe84a4218928e37aaf27875ce5
trunggdungg <le25011985@gmail.com>
2025-03-08 23:46:02 +0700

    add: implement method to find reviews by product ID in ReviewRepository

--- src/main/java/com/example/web_organic/repository/ReviewRepository.java
+++ src/main/java/com/example/web_organic/repository/ReviewRepository.java
@@ -1,9 +1,13 @@
 package com.example.web_organic.repository;
 
 import com.example.web_organic.entity.Review;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
+   List<Review> findByProductId(Integer productId);
 }
