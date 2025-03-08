591bba6c98849a1c339d4d1e8642c353191a89ea
trunggdungg <le25011985@gmail.com>
2025-03-08 23:46:18 +0700

    add: implement methods to retrieve categories by type and slug in CategoryService

--- src/main/java/com/example/web_organic/service/CategoryService.java
+++ src/main/java/com/example/web_organic/service/CategoryService.java
@@ -1,8 +1,25 @@
 package com.example.web_organic.service;
+import com.example.web_organic.entity.Category;
+import com.example.web_organic.modal.Enum.Category_Type;
+import com.example.web_organic.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CategoryService {
+
+@Autowired
+private CategoryRepository categoryRepository;
+
+    public List<Category> getCategoryByTypeAndStatus(Category_Type type) {
+        return categoryRepository.findByTypeAndStatusTrue(type);
+    }
+
+    public Category getCategoryBySlug(String type) {
+        return categoryRepository.findBySlug(type);
+    }
 }
