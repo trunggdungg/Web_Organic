f2649366a7ba4e0c43b5977f954512251b687c59
trunggdungg <le25011985@gmail.com>
2025-03-08 23:46:25 +0700

    add: implement methods to find categories by type and slug in CategoryRepository

--- src/main/java/com/example/web_organic/repository/CategoryRepository.java
+++ src/main/java/com/example/web_organic/repository/CategoryRepository.java
@@ -1,9 +1,15 @@
 package com.example.web_organic.repository;
 
 import com.example.web_organic.entity.Category;
+import com.example.web_organic.modal.Enum.Category_Type;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    List<Category> findByTypeAndStatusTrue(Category_Type type);
+
+    Category findBySlug(String type);
 }
