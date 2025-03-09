e249523f0b60aab4c95ed05ed39780da71039fea
trunggdungg <le25011985@gmail.com>
2025-03-09 23:26:14 +0700

    add: extend BlogRepository to support filtering blogs by category and status

--- src/main/java/com/example/web_organic/repository/BlogRepository.java
+++ src/main/java/com/example/web_organic/repository/BlogRepository.java
@@ -1,12 +1,19 @@
 package com.example.web_organic.repository;
 
 import com.example.web_organic.entity.Blog;
+import com.example.web_organic.entity.Category;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BlogRepository extends JpaRepository<Blog, Integer> {
     Page<Blog> findByStatus(boolean b, PageRequest createdAt);
+
+
+    Page<Blog> findByCategoryAndStatusTrue(Category selectedCategory, Pageable pageable);
 }
