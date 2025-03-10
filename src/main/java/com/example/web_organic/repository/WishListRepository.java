7193efc6d495e4d2d749b3b36a50dc16b56ef0d6
trunggdungg <le25011985@gmail.com>
2025-03-10 22:00:10 +0700

    add: extend WishListRepository with user-specific query methods

--- src/main/java/com/example/web_organic/repository/WishListRepository.java
+++ src/main/java/com/example/web_organic/repository/WishListRepository.java
@@ -1,9 +1,15 @@
 package com.example.web_organic.repository;
 
+import com.example.web_organic.entity.User;
 import com.example.web_organic.entity.WishList;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface WishListRepository extends JpaRepository<WishList, Integer> {
+    List<WishList> findByUser(User currentUser);
+
+    WishList findByUserAndProductId(User currentUser, Integer productId);
 }
