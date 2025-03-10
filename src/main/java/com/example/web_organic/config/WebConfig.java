003b7707cf435dbfba729e126dd0a55294cdf607
trunggdungg <le25011985@gmail.com>
2025-03-10 21:59:57 +0700

    add: integrate AuthenticationInterceptor for user login checks in API routes

--- src/main/java/com/example/web_organic/config/WebConfig.java
+++ src/main/java/com/example/web_organic/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.example.web_organic.config;
 
+import com.example.web_organic.config.interceptor.AuthenticationInterceptor;
 import com.example.web_organic.config.interceptor.CategoryInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -12,9 +13,26 @@ public class WebConfig implements WebMvcConfigurer {
     @Autowired
     private CategoryInterceptor categoryInterceptor;
 
+    @Autowired
+    private AuthenticationInterceptor authenticationInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(categoryInterceptor)
             .addPathPatterns("/**"); // Áp dụng cho tất cả URL
+
+
+//        đây là interceptor để kiểm tra xem người dùng đã đăng nhập chưa
+        registry.addInterceptor(authenticationInterceptor)
+            .addPathPatterns(
+                "/api/reviews",
+
+                "/api/reviews/**",
+                "/api/blogs/**",
+                "/thong-tin-ca-nhan",
+                "/sanpham-yeu-thich"
+                // Không có api là trả về giao diện, có api là trả về JSON
+            );
+
     }
 }
