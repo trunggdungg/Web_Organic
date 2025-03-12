c6d096fa8d26fb10fe530cb3a7f10b90692fd1a5
trunggdungg <le25011985@gmail.com>
2025-03-12 22:39:35 +0700

    add: create ErrorResponse class for standardized error handling

---
+++ src/main/java/com/example/web_organic/modal/response/ErrorResponse.java
@@ -0,0 +1,18 @@
+package com.example.web_organic.modal.response;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ErrorResponse {
+    HttpStatus status;
+
+    Object message;
+    // đổi từ string mess sang object mess để đúng vơis kiểu dữ liệu của message trong các exception
+}
