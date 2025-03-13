135617618f4ea963d7a138d6d5289ad9dce81b72
trunggdungg <le25011985@gmail.com>
2025-03-13 23:30:50 +0700

    add: implement updateCartItem and addToCart methods for cart item management

--- src/main/java/com/example/web_organic/service/CartItemService.java
+++ src/main/java/com/example/web_organic/service/CartItemService.java
@@ -1,13 +1,17 @@
 package com.example.web_organic.service;
 import com.example.web_organic.entity.CartItem;
 import com.example.web_organic.entity.Product;
+import com.example.web_organic.entity.ProductVariants;
 import com.example.web_organic.entity.User;
+import com.example.web_organic.modal.request.AddToCartRequest;
+import com.example.web_organic.modal.request.UpdateCartRequest;
 import com.example.web_organic.repository.CartItemRepository;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,6 +29,32 @@ public class CartItemService {
 
     public void deleteCartItem(Integer id) {
         cartItemRepository.deleteById(id);
+    }
+
+    public void updateCartItem(Integer id, Integer quantity) {
+        cartItemRepository.findById(id).ifPresent(cartItem -> {
+            cartItem.setQuantity(quantity);
+            cartItemRepository.save(cartItem);
+        });
+    }
+
+    public void addToCart(User currentUser, AddToCartRequest addToCartRequest) {
+        ProductVariants productVariant = new ProductVariants();
+        productVariant.setId(addToCartRequest.getProductVariantId());
+// lấy quantity từ request
+
+        CartItem cartItem = cartItemRepository.findByUserAndProductVariant(currentUser, productVariant)
+            .orElse(CartItem.builder()
+                .user(currentUser)
+                .productVariant(productVariant)
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .quantity(0)
+                .build());
+
+        cartItem.setQuantity(cartItem.getQuantity() + addToCartRequest.getQuantity());
+        cartItemRepository.save(cartItem);
+
     }
 
 //  lấy danh sách sản phẩm trong giỏ hàng
