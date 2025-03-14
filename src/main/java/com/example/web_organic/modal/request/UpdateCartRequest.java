2a16e7f703bf6e7cc179c5d6950d110427742eda
trunggdungg <le25011985@gmail.com>
2025-03-14 22:47:15 +0700

    refactor: add UpdateCartRequest class for cart update functionality

---
+++ src/main/java/com/example/web_organic/modal/request/UpdateCartRequest.java
@@ -0,0 +1,14 @@
+package com.example.web_organic.modal.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UpdateCartRequest {
+    Integer quantity;
+}
