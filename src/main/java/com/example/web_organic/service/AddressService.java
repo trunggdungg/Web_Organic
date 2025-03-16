1a503faadb762664075b54fe13c91b4585942567
trunggdungg <le25011985@gmail.com>
2025-03-16 22:11:10 +0700

    refactor: add method to retrieve addresses by user ID in AddressService

--- src/main/java/com/example/web_organic/service/AddressService.java
+++ src/main/java/com/example/web_organic/service/AddressService.java
@@ -1,9 +1,22 @@
 package com.example.web_organic.service;
 
+import com.example.web_organic.entity.Address;
+import com.example.web_organic.repository.AddressRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class AddressService {
+    @Autowired
+    private AddressRepository addressRepository;
+    public List<Address> getAddressByUserId(Integer id) {
+        return addressRepository.findByUserId(id);
+    }
+
+
 }
