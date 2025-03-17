dfffe917d4063eb2623719b345608febb309617c
trunggdungg <le25011985@gmail.com>
2025-03-17 22:54:38 +0700

    feat: add methods to find and delete cart items by user and product variant

--- src/main/java/com/example/web_organic/repository/CartItemRepository.java
+++ src/main/java/com/example/web_organic/repository/CartItemRepository.java
@@ -1,13 +1,19 @@
 package com.example.web_organic.repository;
 
 import com.example.web_organic.entity.CartItem;
+import com.example.web_organic.entity.ProductVariants;
 import com.example.web_organic.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Integer> {
     List<CartItem> findByUser(User user);
+
+    Optional<CartItem> findByUserAndProductVariant(User currentUser, ProductVariants productVariant);
+
+    void deleteByUser(User currentUser);
 }
