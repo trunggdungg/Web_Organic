9f0bcaf40992b9d962a575504919bc22612b90f2
trunggdungg <le25011985@gmail.com>
2025-03-17 22:55:28 +0700

    feat: add method to find orders by status with pagination support

--- src/main/java/com/example/web_organic/repository/OrderRepository.java
+++ src/main/java/com/example/web_organic/repository/OrderRepository.java
@@ -1,9 +1,13 @@
 package com.example.web_organic.repository;
 
 import com.example.web_organic.entity.Order;
+import com.example.web_organic.modal.Enum.Status_Enum;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
+    Page<Order> findAllByStatus(Status_Enum statusEnum, Pageable pageable);
 }
