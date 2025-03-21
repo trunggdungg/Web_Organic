31a86502bfabef8a0a89e0f5cf57d0b046b14024
trunggdungg <le25011985@gmail.com>
2025-03-21 23:25:38 +0700

    feat: add methods for retrieving orders by user and order ID with status filtering

--- src/main/java/com/example/web_organic/repository/OrderRepository.java
+++ src/main/java/com/example/web_organic/repository/OrderRepository.java
@@ -1,13 +1,23 @@
 package com.example.web_organic.repository;
 
 import com.example.web_organic.entity.Order;
+import com.example.web_organic.entity.User;
 import com.example.web_organic.modal.Enum.Status_Enum;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
     Page<Order> findAllByStatus(Status_Enum statusEnum, Pageable pageable);
+
+    Optional<Order> findOrderById(String orderId);
+
+    List<Order> findByUserAndStatus(User user, Status_Enum statusEnum);
+
+    List<Order> findOrderByIdAndUserAndStatus(String orderId, User user, Status_Enum statusEnum);
 }
