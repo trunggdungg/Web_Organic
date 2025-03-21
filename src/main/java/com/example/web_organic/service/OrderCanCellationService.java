209f910334cdad4d7a54885610bc6a8ed3b26b6b
trunggdungg <le25011985@gmail.com>
2025-03-21 23:25:30 +0700

    feat: add method to retrieve order cancellations by user

--- src/main/java/com/example/web_organic/service/OrderCanCellationService.java
+++ src/main/java/com/example/web_organic/service/OrderCanCellationService.java
@@ -1,8 +1,19 @@
 package com.example.web_organic.service;
+import com.example.web_organic.entity.OrderCancellation;
+import com.example.web_organic.entity.User;
+import com.example.web_organic.repository.OrderCanCellationRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class OrderCanCellationService {
+    @Autowired
+    private OrderCanCellationRepository orderCancellationRepository;
+    public List<OrderCancellation> getAllByUser(User user) {
+        return orderCancellationRepository.findByUser(user);
+    }
 }
