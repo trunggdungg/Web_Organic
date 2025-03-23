d061847b37d68daf80830d621bb1b9752a0d5457
trunggdungg <le25011985@gmail.com>
2025-03-23 23:01:44 +0700

    feat: add method to retrieve order cancellations by user

--- src/main/java/com/example/web_organic/repository/OrderCanCellationRepository.java
+++ src/main/java/com/example/web_organic/repository/OrderCanCellationRepository.java
@@ -1,9 +1,13 @@
 package com.example.web_organic.repository;
 
 import com.example.web_organic.entity.OrderCancellation;
+import com.example.web_organic.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderCanCellationRepository extends JpaRepository<OrderCancellation, Integer> {
+    List<OrderCancellation> findByUser(User user);
 }
