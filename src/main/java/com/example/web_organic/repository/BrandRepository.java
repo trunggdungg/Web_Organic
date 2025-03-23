c868c0b8c667bb0022c5be37627d1e1d7a4c1647
trunggdungg <le25011985@gmail.com>
2025-03-23 23:01:09 +0700

    feat: add method to retrieve brands by status

--- src/main/java/com/example/web_organic/repository/BrandRepository.java
+++ src/main/java/com/example/web_organic/repository/BrandRepository.java
@@ -4,6 +4,9 @@ import com.example.web_organic.entity.Brand;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BrandRepository extends JpaRepository<Brand, Integer> {
+    List<Brand> findByStatus(boolean b);
 }
