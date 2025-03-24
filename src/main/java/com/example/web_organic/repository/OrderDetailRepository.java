90a3247e4253bc6401bdba810fab6d2cdebbcfdc
trunggdungg <le25011985@gmail.com>
2025-03-24 23:51:11 +0700

    feat: add method to retrieve order details by order ID

--- src/main/java/com/example/web_organic/repository/OrderDetailRepository.java
+++ src/main/java/com/example/web_organic/repository/OrderDetailRepository.java
@@ -1,9 +1,14 @@
 package com.example.web_organic.repository;
 
+import com.example.web_organic.entity.Order;
 import com.example.web_organic.entity.OrderDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderDetailRepository extends JpaRepository<OrderDetail, Integer> {
+
+    List<OrderDetail> findByOrderId(String id);
 }
