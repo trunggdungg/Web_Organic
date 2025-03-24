5bed29b418ecc1aa68bd316503de4d4e5e0779a5
trunggdungg <le25011985@gmail.com>
2025-03-24 23:51:19 +0700

    feat: add methods to retrieve all order details and details by order ID

--- src/main/java/com/example/web_organic/service/OrderDetailService.java
+++ src/main/java/com/example/web_organic/service/OrderDetailService.java
@@ -1,8 +1,25 @@
 package com.example.web_organic.service;
+import com.example.web_organic.entity.Order;
+import com.example.web_organic.entity.OrderDetail;
+import com.example.web_organic.repository.OrderDetailRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class OrderDetailService {
+    @Autowired
+    private OrderDetailRepository orderDetailRepository;
+
+
+    public List<OrderDetail> getAll() {
+        return orderDetailRepository.findAll();
+    }
+
+    public List<OrderDetail> getOrderDetailsByOrderId(String id) {
+        return orderDetailRepository.findByOrderId(id);
+    }
 }
