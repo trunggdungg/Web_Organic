de795d5140ce35338e8a21ebfc9665f5b0758fbb
trunggdungg <le25011985@gmail.com>
2025-03-27 23:19:20 +0700

    feat: add functionality to count user addresses and unset default addresses

--- src/main/java/com/example/web_organic/repository/AddressRepository.java
+++ src/main/java/com/example/web_organic/repository/AddressRepository.java
@@ -1,7 +1,9 @@
 package com.example.web_organic.repository;
 
 import com.example.web_organic.entity.Address;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -21,4 +23,11 @@ public interface AddressRepository extends JpaRepository<Address, Integer> {
     List<Address> findByIsDefaultTrue();
 
     List<Address> findByUserId(Integer id);
+
+    Integer countByUserId(Integer id);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Address a SET a.isDefault = false WHERE a.user.id = :userId")
+    void unsetDefaultAddresses(@Param("userId") Integer userId);
 }
