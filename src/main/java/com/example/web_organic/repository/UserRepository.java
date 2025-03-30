7ac5720b6fdd4731dff2aaecc69adfe4b04cc24d
trunggdungg <le25011985@gmail.com>
2025-03-31 00:00:45 +0700

    feat: add pagination support for user retrieval in UserRepository

--- src/main/java/com/example/web_organic/repository/UserRepository.java
+++ src/main/java/com/example/web_organic/repository/UserRepository.java
@@ -1,7 +1,12 @@
 package com.example.web_organic.repository;
 
 import com.example.web_organic.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,4 +17,8 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmailAndIsActivatedTrue(String email);
 
     Optional<User> findByEmail(String email);
+
+    @Query("SELECT u FROM User u")
+    Page<User> findAllUser(Pageable pageable);
+
 }
