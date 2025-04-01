b51d8345f8876bd4e0b46ea950caea9f3c0a7435
trunggdungg <le25011985@gmail.com>
2025-04-01 23:45:09 +0700

    feat: add FileResponse class for handling file response data

---
+++ src/main/java/com/example/web_organic/modal/response/FileResponse.java
@@ -0,0 +1,14 @@
+package com.example.web_organic.modal.response;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class FileResponse {
+    String url;
+}
