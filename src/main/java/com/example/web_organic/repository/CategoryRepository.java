1bdafeed5aeb9210c75dfef35d8d5c3861383316
trunggdungg <le25011985@gmail.com>
2025-04-02 22:23:03 +0700

    feat: add method to find category by name in CategoryRepository

--- src/main/java/com/example/web_organic/repository/CategoryRepository.java
+++ src/main/java/com/example/web_organic/repository/CategoryRepository.java
@@ -12,4 +12,6 @@ public interface CategoryRepository extends JpaRepository<Category, Integer> {
     List<Category> findByTypeAndStatusTrue(Category_Type type);
 
     Category findBySlug(String type);
+
+    Category findByName(String name);
 }
