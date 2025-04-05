19a6b69c4c11b8ff069f808dc1f86abdec0d1212
trunggdungg <le25011985@gmail.com>
2025-04-05 19:49:33 +0700

    feat: add REFUNDED status to Payment_Status_Enum for enhanced payment tracking

--- src/main/java/com/example/web_organic/modal/Enum/Payment_Status_Enum.java
+++ src/main/java/com/example/web_organic/modal/Enum/Payment_Status_Enum.java
@@ -1,8 +1,9 @@
 package com.example.web_organic.modal.Enum;
 
 public enum Payment_Status_Enum {
-    PENDING,
-    PAID,
-    CANCELED,
-    FAILED
+    PENDING,// Chờ thanh toán
+    PAID,// Đã thanh toán
+    CANCELED,// Đã hủy
+    FAILED,// Đã thất bại
+    REFUNDED // Đã hoàn tiền
 }
