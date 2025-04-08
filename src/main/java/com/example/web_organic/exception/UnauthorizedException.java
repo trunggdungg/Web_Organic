93dc69289d955bf537378d3bd270776670128de2
trunggdungg <le25011985@gmail.com>
2025-04-08 23:47:18 +0700

    feat: add UnauthorizedException for handling unauthorized access errors

---
+++ src/main/java/com/example/web_organic/exception/UnauthorizedException.java
@@ -0,0 +1,9 @@
+package com.example.web_organic.exception;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+
+
+}
