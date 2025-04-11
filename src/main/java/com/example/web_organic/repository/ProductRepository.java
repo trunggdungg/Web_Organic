5fa02c13769fe6cdafd58c872f3842e93b9fd05d
trunggdungg <le25011985@gmail.com>
2025-04-11 23:43:15 +0700

    feat: enhance ProductRepository with new queries for product sales and category distribution

--- src/main/java/com/example/web_organic/repository/ProductRepository.java
+++ src/main/java/com/example/web_organic/repository/ProductRepository.java
@@ -9,9 +9,12 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
@@ -27,7 +30,7 @@ public interface ProductRepository extends JpaRepository<Product, Integer>, JpaS
 //    lấy ra các sản phẩm theo category
     Page<Product> findByCategoryId(Integer categoryId, Pageable pageable);
 //    tim theo slug
-    Page<Product> findByCategorySlug(String slug, Pageable pageable);
+    Page<Product> findByCategorySlugAndStatusTrue(String slug, Pageable pageable);
 
     List<Product> findTop15ByOrderByDiscountDesc();
 
@@ -39,4 +42,43 @@ public interface ProductRepository extends JpaRepository<Product, Integer>, JpaS
     List<Product> findByCategoryIdAndIdNotAndStatusTrue(Integer categoryId, Integer excludeProductId);
 
     Page<Product> findByCategoryAndStatusTrue(Category selectedCategory, Pageable pageable);
+
+    Product getProductById(Integer productId);
+
+
+    @Query("SELECT SUM(od.quantity) FROM OrderDetail od JOIN od.order o " +
+        "WHERE o.status = 'COMPLETED' AND o.orderDate >= :startDate AND o.orderDate < :endDate")
+    Integer countSoldProductsBetween(@Param("startDate") LocalDateTime startDate,
+                                     @Param("endDate") LocalDateTime endDate); //  Tổng hợp doanh thu theo sản phẩm
+
+    // Dữ liệu cho biểu đồ phân loại sản phẩm
+    @Query("""
+    SELECT c.name AS categoryName,
+           COUNT(DISTINCT pv.product.id) AS productCount,
+           SUM(od.quantity) AS totalQuantitySold
+    FROM OrderDetail od
+    JOIN od.productVariant pv
+    JOIN pv.product p
+    JOIN p.category c
+    JOIN od.order o
+    WHERE o.status = 'COMPLETED'
+    GROUP BY c.id, c.name
+    ORDER BY totalQuantitySold DESC
+    """)
+    List<Map<String, Object>> getProductCategoryDistribution();
+
+
+    @Query("SELECT p.name AS productName, SUM(od.quantity) AS totalQuantitySold " +
+        "FROM OrderDetail od " +
+        "JOIN od.productVariant pv " +
+        "JOIN pv.product p " +
+        "JOIN od.order o " +
+        "WHERE o.status = 'COMPLETED' AND MONTH(o.orderDate) = MONTH(CURRENT_DATE) AND YEAR(o.orderDate) = YEAR(CURRENT_DATE) " +
+        "GROUP BY p.id, p.name " +
+        "ORDER BY totalQuantitySold DESC")
+    List<Map<String, Object>> getHotProducts(Pageable pageable);    // Dữ liệu cho biểu đồ sản phẩm bán chạy
+
+
+
+
 }
