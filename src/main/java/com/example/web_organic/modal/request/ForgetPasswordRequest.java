010c0802b841161030abb9dfe56ac69b51df8fee
trunggdungg <le25011985@gmail.com>
2025-04-13 23:44:18 +0700

    feat: add ForgetPasswordRequest class for handling password reset requests

---
+++ src/main/java/com/example/web_organic/modal/request/ForgetPasswordRequest.java
@@ -0,0 +1,14 @@
+package com.example.web_organic.modal.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE)
+public class ForgetPasswordRequest {
+    String email;
+}
