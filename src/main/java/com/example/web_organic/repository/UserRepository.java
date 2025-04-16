93394bb8532b1d35c77514a42427434e09c57053
trunggdungg <le25011985@gmail.com>
2025-04-16 22:50:29 +0700

    feat: add methods for user retrieval and new customer count in UserRepository

--- src/main/java/com/example/web_organic/repository/UserRepository.java
+++ src/main/java/com/example/web_organic/repository/UserRepository.java
@@ -1,14 +1,18 @@
 package com.example.web_organic.repository;
 
 import com.example.web_organic.entity.User;
+import com.example.web_organic.modal.Enum.User_Role;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -21,4 +25,14 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     @Query("SELECT u FROM User u")
     Page<User> findAllUser(Pageable pageable);
 
+    User findUserById(Integer id);
+
+    @Query("SELECT COUNT(c) FROM User c WHERE c.createdAt >= :startDate AND c.createdAt < :endDate")
+    int countNewCustomersBetween(@Param("startDate") LocalDateTime startDate,
+                                 @Param("endDate") LocalDateTime endDate);
+
+    //khách hàng mới
+    List<User> findTop5AndStatusTrueByOrderByCreatedAtDesc();
+
+   User findByRole(User_Role userRole);
 }
