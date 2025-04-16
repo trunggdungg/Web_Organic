8c54193491f26cda2ab6efb6440f8e2d4fe15cad
trunggdungg <le25011985@gmail.com>
2025-04-16 22:50:48 +0700

    feat: add admin path pattern to authentication interceptor in WebConfig

--- src/main/java/com/example/web_organic/config/WebConfig.java
+++ src/main/java/com/example/web_organic/config/WebConfig.java
@@ -26,7 +26,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(authenticationInterceptor)
             .addPathPatterns(
                 "/api/reviews",
-
+                "/admin/**",
                 "/api/reviews/**",
                 "/api/blogs/**",
                 "/thong-tin-ca-nhan",
