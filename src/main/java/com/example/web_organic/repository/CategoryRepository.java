61c6fa2bbcb7398a3b126cc10b167b7f03ad36c3
trunggdungg <le25011985@gmail.com>
2025-04-23 23:33:55 +0700

    feat: add queries to count active, inactive, and new categories in CategoryRepository

--- src/main/java/com/example/web_organic/repository/CategoryRepository.java
+++ src/main/java/com/example/web_organic/repository/CategoryRepository.java
@@ -3,6 +3,7 @@ package com.example.web_organic.repository;
 import com.example.web_organic.entity.Category;
 import com.example.web_organic.modal.Enum.Category_Type;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,4 +15,16 @@ public interface CategoryRepository extends JpaRepository<Category, Integer> {
     Category findBySlug(String type);
 
     Category findByName(String name);
+
+    // Đếm số danh mục đang hoạt động
+    @Query("SELECT COUNT(c) FROM Category c WHERE c.status = true")
+    int countActiveCategories();
+
+    // Đếm số danh mục không hoạt động
+    @Query("SELECT COUNT(c) FROM Category c WHERE c.status = false")
+    int countInactiveCategories();
+
+    // Đếm số danh mục mới trong tháng hiện tại
+    @Query("SELECT COUNT(c) FROM Category c WHERE MONTH(c.createdAt) = MONTH(CURRENT_DATE) AND YEAR(c.createdAt) = YEAR(CURRENT_DATE)")
+    int countNewCategoriesCurrentMonth();
 }
