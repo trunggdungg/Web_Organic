3e4592df7fcf01817ded0f324580fb31662fc286
trunggdungg <le25011985@gmail.com>
2025-04-29 21:36:41 +0700

    feat: implement contact message sending functionality with repository integration

--- src/main/java/com/example/web_organic/service/ContactService.java
+++ src/main/java/com/example/web_organic/service/ContactService.java
@@ -1,8 +1,24 @@
 package com.example.web_organic.service;
+import com.example.web_organic.entity.Contact;
+import com.example.web_organic.modal.request.InsertContactRequest;
+import com.example.web_organic.repository.ContactRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ContactService {
+    @Autowired
+    private ContactRepository contactRepository;
+    public void sendContactMessage(InsertContactRequest insertContactRequest) {
+          Contact contact = Contact.builder()
+                .fullName(insertContactRequest.getName())
+                .email(insertContactRequest.getEmail())
+                .phone(insertContactRequest.getPhone())
+                .title(insertContactRequest.getSubject())
+                .message(insertContactRequest.getMessage())
+                .build();
+        contactRepository.save(contact);
+    }
 }
